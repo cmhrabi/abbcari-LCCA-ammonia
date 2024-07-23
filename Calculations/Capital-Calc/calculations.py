14a7ad7e7557d6c23461df8a094f5aafa1ad7a80
aifram <aifram@gmail.com>
2024-07-22 21:30:32 -0400

    capex calc

--- Calculations/Capital-Calc/calculations.py
+++ Calculations/Capital-Calc/calculations.py
@@ -79,7 +79,7 @@ def C_s_energy_calc(PV_opex, t_f, t_o, C_s):
     C_s_bar_sum = {}
     PV_sum = 0
     i = 0
-
+    # *** not sure
     for key in C_s.keys():
         C_s_bar_sum[key] = C_s_bar[key] + C_s[key] * PV_opex[i]
         PV_sum += PV_opex[i]
@@ -92,11 +92,53 @@ def C_s_energy_calc(PV_opex, t_f, t_o, C_s):
 
 def ER_calc(ER_b, X_n, X_b, eff, alpha_j):
     ER = [[{}]]
-    for i in range(len(X_n)):
+    for i in range(len(X_n)):  # here i is the year where as in our paper it is tech i ***NEED TO FIX
         for j in range(len(ER_b)):
             for key in ER_b[j].keys():
+                if i != 0:
+                    ER[i][j][key] = ((1 - eff) * ER_b[i][j][key] * math.pow(X_n[i] / X_b, alpha_j) + eff * ER_b[i][j][
+                        key]) * ((X_n[i] - X_n[i - 1]) / X_b)
+                else:
+                    # ask not sure
+                    ER[i][j][key] = (
+                                (1 - eff) * ER_b[i][j][key] * math.pow(X_n[i] / X_b, alpha_j) + eff * ER_b[i][j][key])
+    return ER
 
 
+def C_j_s_calc(ER, T_op, C_s_bar):
+    # here ER is given by tech so it is [{}] not [[{}]]
+    C_j_s = [{}]
+
+    for j in range(len(ER)):
+        for key in C_s_bar.keys():
+            C_j_s[j][key] = ER[j][key] * C_s_bar[key] * T_op
+
+    return C_j_s
+
+
+def C_energy_calc(C_j_s):
+    C_energy = 0
+
+    for j in range(len(C_j_s)):
+        for key in C_j_s[j].keys():
+            C_energy += C_j_s[j][key]
+
+    return C_energy
+
+
+def C_H2O_bar_calc(C_H2O, T_op, WR):
+    C_H2O_bar = WR * C_H2O * T_op
+    return C_H2O_bar
+
+
+def C_opex_bar_calc(C_energy, C_H2O, C_opex_add, theta):
+    C_opex_bar = (C_energy + C_H2O + C_opex_add) * (1 / (1 - theta))
+    return C_opex_bar
+
+
+def C_capex(PV_opex, C_opex_bar):
+    C_capex = PV_opex * C_opex_bar
+    return C_capex
 
 
 def main():
@@ -123,14 +165,26 @@ def main():
     T_op = 8000
     eff = 0
     ER_b = [
-        {
-            "elec": [200, 350],
-            "nat": [300, 500]
-        },
-        {
-            "elec": [150, 200],
-            "nat": [100, 150]
-        }
+        [
+            {
+                "elec": [200, 350],
+                "nat": [300, 500]
+            },
+            {
+                "elec": [150, 200],
+                "nat": [100, 150]
+            }
+        ],
+        [
+            {
+                "elec": [200, 350],
+                "nat": [300, 500]
+            },
+            {
+                "elec": [150, 200],
+                "nat": [100, 150]
+            }
+        ]
     ]
     C_s = {
         "elec": [200, 350],
@@ -138,6 +192,8 @@ def main():
     }
     WR = [100, 150]
     C_H2O = 100
+    C_opex_add = 0
+    theta = 0.1  # marketing and distribution factor
 
     # Calc all X_n
     X_n, X_n_cumulative = X_n_calc(X_sat, X_b, t_o, t_f)
