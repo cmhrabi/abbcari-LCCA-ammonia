9b7af26d79098e3739e427a7786b4e934c3cf3a5
cmhrabi <calum.hrabi@gmail.com>
2024-08-29 22:00:55 -0400

    Added docker to run db and backend, added ORM to handle DB reqs (Not Working)

--- backend/backend/app.py
+++ backend/backend/app.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask import Flask, request, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
+from os import environ
+
+from backend.api.users import hello_world
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URL')
+db = SQLAlchemy(app)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def json(self):
+        return {'id': self.id,'username': self.username, 'email': self.email}
+
+with app.app_context():
+    db.create_all()
