1e59e7573f863efb29f363ae84f513e71143752d
cmhrabi <calum.hrabi@gmail.com>
2024-09-09 17:33:45 -0400

    Added storybook to project

---
+++ frontend/src/components/Button/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Button = () => {
+  return (
+    <div className="Button">
+      <button>LCCA Main Button</button>
+    </div>
+  );
+}
+
+export default Button;
