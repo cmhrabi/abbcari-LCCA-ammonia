ab52686ee20c47bbf8f8fb0607c3af7bd030d04e
cmhrabi <83670707+cmhrabi@users.noreply.github.com>
2024-12-07 21:05:01 -0500

    SSP-79: Finish Start Analysis page + modal (#10)

---
+++ frontend/src/design/Breadcumbs/Breadcrumbs.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import {
+  Breadcrumbs as NextBreadcrumbs,
+  BreadcrumbItem,
+} from "@nextui-org/react";
+import { useNavigate } from "react-router-dom";
+
+interface BreadcrumbsProps {
+  items: { label: string; link: string }[];
+}
+
+const Breadcrumbs: React.FC<BreadcrumbsProps> = ({ items }) => {
+  const navigate = useNavigate();
+
+  const navigateTo = (link: string) => {
+    if (link) {
+      navigate(link);
+    }
+  };
+
+  return (
+    <div className="pb-8">
+      <NextBreadcrumbs>
+        {items.map((item, index) => (
+          <BreadcrumbItem
+            classNames={{
+              item: "text-breadcrumb underline data-[current=true]:text-breadcrumb-final data-[current=true]:no-underline",
+            }}
+            size="sm"
+            key={index}
+            onClick={() => navigateTo(item.link)}
+          >
+            {item.label}
+          </BreadcrumbItem>
+        ))}
+      </NextBreadcrumbs>
+    </div>
+  );
+};
+
+export default Breadcrumbs;
