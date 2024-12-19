82c1ca6398c12ea460246eba9bc70e4405a022f4
cmhrabi <83670707+cmhrabi@users.noreply.github.com>
2024-12-19 17:33:19 -0500

    SSP-81: Implement Redux (#11)

---
+++ frontend/src/Slices/nameSlice.ts
@@ -0,0 +1,36 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+export interface NameState {
+  value: {
+    analysisName: string;
+    tech1Name: string;
+    tech2Name: string;
+    type: string;
+  };
+}
+
+export const nameSlice = createSlice({
+  name: "name",
+  initialState: {
+    value: { analysisName: "", tech1Name: "", tech2Name: "", type: "" },
+  },
+  reducers: {
+    setAnalysisName: (state, action: PayloadAction<string>) => {
+      state.value.analysisName = action.payload;
+    },
+    setTech1Name: (state, action: PayloadAction<string>) => {
+      state.value.tech1Name = action.payload;
+    },
+    setTech2Name: (state, action: PayloadAction<string>) => {
+      state.value.tech2Name = action.payload;
+    },
+    setType: (state, action: PayloadAction<string>) => {
+      state.value.type = action.payload;
+    },
+  },
+});
+
+export const { setAnalysisName, setTech1Name, setTech2Name, setType } =
+  nameSlice.actions;
+
+export default nameSlice.reducer;
